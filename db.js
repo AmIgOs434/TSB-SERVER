2648185ef788040a75e8c89ea9996c1cd6fddf8b
AmIgOs434 <104652966+AmIgOs434@users.noreply.github.com>
2023-07-15 10:43:02 +0300

    Update db.js

--- db.js
+++ db.js
@@ -4,9 +4,15 @@ module.exports = new Sequelize(
     process.env.DB_NAME, // Название БД
     process.env.DB_USER, // Пользователь
     process.env.DB_PASSWORD, // ПАРОЛЬ
+    
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        dialectOptions: {
+            ssl: {
+              require: true,
+           
+            }}
     }
+
 )
